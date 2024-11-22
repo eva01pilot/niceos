9af4806e2ea711bf6c3ea92e87f51e4d587a515e
Ilya <eva01pilothmm@gmail.com>
2024-11-22 10:40:13 +0300

    Volar

--- nixvim.nix
+++ nixvim.nix
@@ -154,29 +154,27 @@
           enable = true;
         };
         ts_ls = {
-          enable = true;
+          enable = false;
           filetypes = [
             "javascript"
             "javascriptreact"
             "typescript"
             "typescriptreact"
-            "vue"
           ];
-          extraOptions = {
-            init_options = {
-              plugins = [
-                {
-                  name = "@vue/typescript-plugin";
-                  location = "${lib.getBin pkgs.vue-language-server}/lib/node_modules/@vue/language-server";
-                  languages = ["vue"];
-                }
-              ];
-            };
-          };
         };
         volar = {
           enable = true;
           package = pkgs.vue-language-server;
+          filetypes = [
+              "typescript" "javascript" "javascriptreact" "typescriptreact" "vue"
+          ];
+          extraOptions = {
+            initOptions = {
+              vue = {
+                hybridMode = false;
+              };
+            };
+          };
         };
       };
     };
