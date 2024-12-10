b012559f29f1ac6d2daf65c84008e0f5dac108ab
Ilya <eva01pilothmm@gmail.com>
2024-12-10 09:28:56 +0300

    fix

--- zellij/zellij.nix
+++ zellij/zellij.nix
@@ -2,16 +2,17 @@
   programs.zellij = {
     enable = true;
     enableFishIntegration = true;
-    xdg.configFile."zellij/config.kdl".text = ''
-      keybinds {
-          // keybinds are divided into modes
-          normal {
-              // bind instructions can include one or more keys (both keys will be bound separately)
-              // bind keys can include one or more actions (all actions will be performed with no sequential guarantees)
-              bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
-              bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
-          }
-      }
-    '';
   };
+
+  xdg.configFile."zellij/config.kdl".text = ''
+    keybinds {
+        // keybinds are divided into modes
+        normal {
+            // bind instructions can include one or more keys (both keys will be bound separately)
+            // bind keys can include one or more actions (all actions will be performed with no sequential guarantees)
+            bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
+            bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
+        }
+    }
+  '';
 }
